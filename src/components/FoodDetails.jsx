56642d26db71459610897c4edac366fea33318a4
chogocorban <156567997+chogocorban@users.noreply.github.com>
2024-06-13 11:27:26 +0300

    Food Recipe App

---
+++ src/components/FoodDetails.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import styles from "./fooddetails.module.css";
+import ItemList from "./ItemList";
+
+export default function FoodDetails({ foodId }) {
+  const [food, setFood] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "3a79e9ae3fa24ae9ba50f6062e400be7";
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFood(data);
+      setIsLoading(false);
+    }
+    fetchFood();
+  }, [foodId]);
+  return (
+    <div>
+      <div className={styles.recipeCard}>
+        <h1 className={styles.recipeName}>{food.title}</h1>
+        <img className={styles.recipeImage} src={food.image} alt="" />
+        <div className={styles.recipeDetails}>
+          <span>
+            <strong>⏲️{food.readyInMinutes} Minutes </strong>
+          </span>
+          <span>
+            👨‍👩‍👧‍👦 <strong>Serves {food.servings}</strong>
+          </span>
+          <span>
+            <strong>
+              {food.vegetarian ? " 🥕 Vegetarian" : " 🍖 Non-Vegeratian"}
+            </strong>
+          </span>
+          <strong>
+            <span>{food.vegan ? " 🐮 Vegan" : ""}</span>
+          </strong>
+        </div>
+        <div>
+          💲<span>{food.pricePerServing / 100} Per serving</span>
+        </div>
+        <h2>Ingredients</h2>
+        <ItemList food={food} isLoading={isLoading} />
+        <h2>Instructions</h2>
+        <div className={styles.recipeInstructions}>
+          <ol>
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              food.analyzedInstructions[0].steps.map((step) => (
+                <li>{step.step}</li>
+              ))
+            )}
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import styles from "./search.module.css";
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "3a79e9ae3fa24ae9ba50f6062e400be7";
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("pizza");
+  //Syntax of the useEffect hook
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
+    }
+    fetchFood();
+  }, [query]);
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.input}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
+}
